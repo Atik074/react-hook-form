33cf4d1daa81349b06da924a5ff257c7e3806ff4
Atik074 <atiksmats@gmail.com>
2024-01-31 00:03:19 +0600

    form validate by zod

--- src/components/reactHookForm/ReactHookForm.tsx
+++ src/components/reactHookForm/ReactHookForm.tsx
@@ -1,10 +1,18 @@
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import cn from "../../util/cn";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { TNormalForm, signUpShcema , } from "./Validation";
+
+
+
+
 
 const ReactHookForm = () => {
-    const {register , handleSubmit} = useForm()
+    const {register , handleSubmit, formState:{errors}} = useForm<TNormalForm>({
+            resolver:zodResolver(signUpShcema)
+    })
 
-    const onSubmit = (data) =>{
+    const onSubmit = (data:FieldValues) =>{
         console.log(data)
     }
    const double = true;
@@ -23,20 +31,35 @@ const ReactHookForm = () => {
             <div className="w-full max-w-md">
                 <label className="block" htmlFor="name">Name</label>
             <input  type="text" id="name" {...register('name')} />
+            {
+                  errors.name && (
+                        <span className="text-red-500 text-md ml-6">{errors?.name?.message}</span>
+                    )}
+          
             </div>
+            
             <div className="w-full max-w-md">
                 <label className="block" htmlFor="email">Email</label>
                 <input className="border rounded" type="email" id="email" {...register('email')} />
+                {
+                  errors.email && (
+                        <span className="text-red-500 text-md ml-6">{errors?.email?.message}</span>
+                    )}
             </div>
 
-            <div className="w-full max-w-md">
-                <label className="block" htmlFor="name">Contact Number</label>
-                <input className="border rounded" type="number" id="number" {...register('number')} />
-            </div>
+      
+
             <div className="w-full max-w-md">
                 <label className="block" htmlFor="password">Password</label>
                 <input className="border rounded mb-2" type="password" id="password" {...register('password')} />
+                
                 <input  type="checkbox" name="" id="" />
+                {
+                  errors.password && (
+                        <span className="text-red-500 text-md ml-6">{errors?.password?.message}</span>
+                    )}
+
+               
             </div>
            
           
@@ -47,7 +70,7 @@ const ReactHookForm = () => {
           "md:grid-cols-2" : double
         })}>
 
-            <div className="bg-green-400 w-full max-w-md col-start-1 md:col-start-2  flex justify-end">
+            <div className=" w-full max-w-md col-start-1 md:col-start-2  flex justify-end">
                 <button className="bg-orange-500 text-2xl rounded-md px-4 py-1 w-full md:w-fit">Submit</button>
             </div>
          </div>
